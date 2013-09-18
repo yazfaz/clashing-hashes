a72ad66b0618f07dd27dd66cfc0e09d5509f7db2
MakerSquare Student <students@themakersquare.com>
2013-09-18 16:46:36 -0500

    Up to extension.

--- clashing-hashes.rb
+++ clashing-hashes.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
+require_relative 'twitter'
 
 get '/' do
   # '<h1>hello joe and yasmin</h1>'
@@ -14,13 +15,20 @@ end
 post '/twitter' do
   @first_hash = params[:first_hash]
   @second_hash = params[:second_hash]
-  "<p>Your posts #{@first_hash} and #{@second_hash} were submitted</p>"
-  # params.inspect
+  
+  # Require twitter module; create empty tweets array; based off Tweet class in Twitter module, make tweets, push in array. Then in html twitter_results.erb, display array
+  @tweets = []
+  tweet1 = Twitter::Tweet.new("yazfaz", "ready for b12")
+  @tweets << tweet1
+  erb :twitter_results
 
 end
 
 # get '/twitter' do
 #   puts "Your posts #{@first_hash} and #{@second_hash} was submitted"
+# erb:index
 # end
 
 # request.POST.inspect
+
+# http://surrealdetective.github.io/blog/2013/07/01/the-nested-ruby-params-hash-for-complex-html-forms-and-sinatra/
