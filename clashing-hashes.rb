28a2cef064b45cd7a9daac604c0f24dd4c419c93
MakerSquare Student <students@themakersquare.com>
2013-09-18 10:57:57 -0500

    Post form data to twitter.

--- clashing-hashes.rb
+++ clashing-hashes.rb
@@ -4,5 +4,23 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  'hello world'
+  # '<h1>hello joe and yasmin</h1>'
+  @name = 'joe'
+  @show_description = true
+  
+  erb :index
 end
+
+post '/twitter' do
+  @first_hash = params[:first_hash]
+  @second_hash = params[:second_hash]
+  "<p>Your posts #{@first_hash} and #{@second_hash} were submitted</p>"
+  # params.inspect
+
+end
+
+# get '/twitter' do
+#   puts "Your posts #{@first_hash} and #{@second_hash} was submitted"
+# end
+
+# request.POST.inspect
