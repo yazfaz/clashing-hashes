689311f1c1444bd1bc7e6a10b446f4c0c68a0f1b
MakerSquare Student <students@themakersquare.com>
2013-09-20 09:35:30 -0500

    Change input name to login; change params to login (replaced 'hash')

--- clashing-hashes.rb
+++ clashing-hashes.rb
@@ -12,8 +12,8 @@ end
 
 # The params[:first_login] refers to the input form in index.html. So by calling it here, we get the user's login (value passed into form). Here, setting @user1 to a new user with login at params[:first_login]
 post '/twitter' do
-  @user1 = FollowerClash::User.new(params[:first_hash])
-  @user2 = FollowerClash::User.new(params[:second_hash])
+  @user1 = FollowerClash::User.new(params[:first_login])
+  @user2 = FollowerClash::User.new(params[:second_login])
   @result = FollowerClash::Comparer.new(@user1,@user2).compare
   
   erb :twitter_results
